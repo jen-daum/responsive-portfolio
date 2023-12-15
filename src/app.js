ab2c3c4047e542c6d9c417388721023826bd6b6b
jen-daum <jennifer.daum.me@gmail.com>
2023-12-15 11:53:40 +0000

    added background to about section + sorted contact section

--- src/app.js
+++ src/app.js
@@ -3,3 +3,12 @@ document.querySelectorAll(".navbar-nav>li>a").forEach((item) => {
     document.querySelector(".navbar-collapse").classList.remove("show");
   });
 });
+
+window.onload = function () {
+  var img = new Image();
+  img.onload = function () {
+    document.getElementById("about").style.backgroundImage =
+      "url('https://s3.amazonaws.com/shecodesio-production/uploads/files/000/107/288/original/duotone_keyboard_hand.png?1702638653')";
+  };
+  img.src = "pictures/duotone_keyboard_hand.png";
+};
